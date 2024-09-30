4de21153f1a7aeac3f3a24fd92444e1982ce7a11
Clemens Schrattenholzer <cs+git@limewood.at>
2024-09-30 13:45:43 +0200

    Cleared Client

--- sync_service_gateway_client/api/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from sync_service_gateway_client.api.ih_notifications_api import IHNotificationsApi
-from sync_service_gateway_client.api.kontoauszug_api import KontoauszugApi
-from sync_service_gateway_client.api.mahnstufe_api import MahnstufeApi
-from sync_service_gateway_client.api.portal_api import PortalApi
-from sync_service_gateway_client.api.default_api import DefaultApi
-
